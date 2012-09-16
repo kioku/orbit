f3648167295efb7b6f3349f7f106feaabc4ef0f7
Claudiu <klaus@kioku.(none)>
2012-09-16 06:59:34 +0300

    another go

--- web.js
+++ web.js
@@ -2,9 +2,9 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.use(express.static(__dirname + "/js"));
-app.use(express.static(__dirname + "/css"));
-app.use(express.static(__dirname + "/images"));
+app.use("/js", express.static(__dirname + "/js"));
+app.use("/css", express.static(__dirname + "/css"));
+app.use("/images", express.static(__dirname + "/images"));
 
 app.get('/', function(request, response) {
   response.sendfile('game.html');
