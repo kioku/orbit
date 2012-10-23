5ad7d4d574d6a5e0d3643c03ba8b2755fc2744c2
Claudiu <klaus@kioku.(none)>
2012-10-23 23:24:16 +0300

    changed port for heroku

--- web-multi.js
+++ web-multi.js
@@ -5,7 +5,8 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
-var port = 5000;
+//var port = 5000;
+var port = 9333; // heroku port
 server.listen(port, function() {
   console.log("Listening on " + port);
 });
