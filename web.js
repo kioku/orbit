5a34c2d3ddbdf78c8c9985dc7dc9ed8eb40320cd
Claudiu Ivan <contact@claudiu-ivan.com>
2025-03-15 14:38:00 +0200

    snaphost cutoff

--- web.js
+++ web.js
@@ -1,10 +1,15 @@
 var express = require('express');
+var logger = require('morgan');
 
-var app = express.createServer(express.logger());
+
+console.log('what?');
+var app = express();
 
 app.use("/js", express.static(__dirname + "/js"));
 app.use("/css", express.static(__dirname + "/css"));
 app.use("/images", express.static(__dirname + "/images"));
+
+app.use(logger);
 
 app.get('/', function(request, response) {
   response.sendfile('game.html');
@@ -12,5 +17,5 @@ app.get('/', function(request, response) {
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  //console.log("Listening on " + port);
 });
